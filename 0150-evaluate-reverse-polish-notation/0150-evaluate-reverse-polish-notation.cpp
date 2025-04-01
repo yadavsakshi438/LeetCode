e91d0a24426d8e49ed00068870ab0c124d4c6550
yadavsakshi438 <sakshiyadav5409@gmail.com>
2025-04-01 23:01:32 +0530

    Time: 0 ms (100%), Space: 17 MB (64.47%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> s;
-        for (auto a : tokens) {
-            if (a.size() == 1 && !isdigit(a[0])) {  // check if an operator
-                int num2 = s.top();
+        stack<int>s;
+        for(auto a:tokens){
+            if(a.size()==1 && !isdigit(a[0])){
+                int num2=s.top();
                 s.pop();
-                int num1 = s.top();
+                int num1=s.top();
                 s.pop();
-                switch (a[0]) {  // note switch use char or integer
+                switch(a[0]){
                     case '+':
-                        s.push(num1 + num2);
+                        s.push(num1+num2);
                         break;
                     case '-':
-                        s.push(num1 - num2);
+                        s.push(num1-num2);
                         break;
                     case '*':
-                        s.push(num1 * num2);
+                        s.push(num1*num2);
                         break;
                     case '/':
-                        s.push(num1 / num2);
+                        s.push(num1/num2);
                         break;
                 }
-            } else {  // if an number push to stack
+            } else{
                 s.push(atoi(a.c_str()));
             }
         }
