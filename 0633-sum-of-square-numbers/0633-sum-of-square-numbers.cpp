ea6e23be5267e5a22a30db6675ef8444e2568956
yadavsakshi438 <sakshiyadav5409@gmail.com>
2025-06-09 21:38:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.31%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long left = 0;
+        long long right = (long long)sqrt(c); 
+        while (left <= right) {
+            long long sum = left * left + right * right;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                left++;  
+            } else {
+                right--;  
+            }
+        }
+        return false;  
+    }
+};
