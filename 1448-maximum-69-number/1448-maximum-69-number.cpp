be06fd24721a0ffbad35c989339797354325f1d1
yadavsakshi438 <sakshiyadav5409@gmail.com>
2025-08-16 22:43:57 +0530

    Time: 0 ms (100%), Space: 8 MB (41.82%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int> v;
+        while(num>0){
+            v.push_back(num%10);
+            num=num/10;
+        }
+        reverse(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            if(v[i]==6){
+                v[i]=9;
+                break;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<v.size();i++){
+            ans=ans*10+v[i];
+        }
+        return ans;
+    }
+};
