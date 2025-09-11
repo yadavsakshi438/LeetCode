05b726abfa86f25219dd86cf84e05df87fb8d22c
yadavsakshi438 <sakshiyadav5409@gmail.com>
2025-09-11 21:34:25 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.35%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0 || s[0] == '0') return 0;
+        
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;  
+        dp[1] = 1; 
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = stoi(s.substr(i - 1, 1));
+            int twoDigit = stoi(s.substr(i - 2, 2));
+            
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
+    }
+};
